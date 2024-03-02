2d46c7096938c0c2ef1ab16543aee358c3c37349
KooSL <142590197+KooSL@users.noreply.github.com>
2024-03-02 18:00:55 +0545

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,20 @@
 
+<footer>
+        <div class="grid grid-cols-3 gap-15 bg-gray-200 mt-80 py-10 px-80">
+            <div>
+                <h1 class="text-2xl font-bold">About Us</h1>
+                <p class="text-gray-500">we provide an amazing facilities regarding web site </p>
+            </div>
+
+            <div>
+                <h1 class="text-2xl font-bold">Contact</h1>
+                <p class="text-gray-500">contact us for any kind of queries</p>
+            </div>
+        </div>
+
+        <div class="text-center bg-gray-500 text-white p-5">
+            <p>&copy; 2024 All rights reserved</p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=3.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+
+    <nav class="flex bg-blue-700 px-20 items-center justify-between">
+        <a href="index.php"><img src="image/garage.png" alt=""></a>
+        <div>
+            <a href="index.php" class="text-lg font-bold text-white px-5">Home</a>
+            <a href="admin/dashboard.php" class="text-lg font-bold text-white px-5">Dashboard</a>
+            <a href="login.php" class="text-lg font-bold text-white px-5">Login</a>
+        </div>
+    </nav>
