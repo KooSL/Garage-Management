332608b5f13cc969d3bfd80de4821b144ac2903d
KooSL <142590197+KooSL@users.noreply.github.com>
2024-03-03 21:48:05 +0545

    Add files via upload

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php include 'includes/header.php'; ?>
+
+<body class="bg-[url('image/background.png')] bg-cover bg-no-repeat">
+    
+    <div class="pt-10 text-center">
+        <span class="font-bold text-white text-3xl">About Us</span>
+        <h1 class="text-white">Welcome to our Garage Management System</h1>
+        <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel mi vel eros tristique ultrices. Donec nec orci eu est hendrerit rhoncus. Curabitur vestibulum lectus et justo bibendum aliquet. Sed at vestibulum risus.</p>
+    </div>
+
+</body>
+
+<?php include 'includes/footer.php'; ?>
--- contact.php
+++ contact.php
@@ -2,26 +2,21 @@
 
 <body class="bg-[url('image/background.png')] bg-cover bg-no-repeat">
     
-
-
-<div class="">
+<div class="w-80 mx-auto my-20">
     <form action="" method="POST">
-        <p>Contact Us</p>
-        <input type="text" class="w-full p-2 my-5 border-2 border-gray-300" name="cntname" placeholder="Full Name">
-        <input type="email" class="w-full p-2 my-5 border-2 border-gray-300" name="cntemail" placeholder="Email">
-        <input type="text" class="w-full p-2 my-5 border-2 border-gray-300" name="cntphone" placeholder="Phone Number">
-        <textarea type="text" class="w-full p-2 my-5 border-2 border-gray-300" name="cntmessage" placeholder="Message"></textarea>
-        <input type="submit" name="submit" class="w-full p-2 my-5 bg-blue-700 text-white font-bold"></input>
+        <p class="text-white text-2xl font-bold mb-5">Contact Us</p>
+        <input type="text" class="w-full p-2 my-2 border-2 border-gray-300" name="cntname" placeholder="Full Name" required>
+        <input type="email" class="w-full p-2 my-2 border-2 border-gray-300" name="cntemail" placeholder="Email" required>
+        <input type="text" class="w-full p-2 my-2 border-2 border-gray-300" name="cntphone" placeholder="Phone Number">
+        <textarea type="text" class="w-full p-2 my-2 border-2 border-gray-300" name="cntmessage" placeholder="Message" required></textarea>
+        <input type="submit" name="submit" class="w-full p-2 my-2 bg-blue-700 text-white font-bold cursor-pointer hover:bg-gray-600" value="Submit">
     </form>
 </div>
 
 </body>
 
-    
-
         
 <?php include 'includes/footer.php'; ?> 
-
 
 <?php
 if(isset($_POST['submit'])){
@@ -35,7 +30,7 @@ if(isset($_POST['submit'])){
     $result = mysqli_query($conn, $qry);
     include 'includes/closeconnection.php';
     if($result){
-        echo "<script>alert('Submited Successfully');
+        echo "<script>alert('Submitted Successfully');
             window.location.href = 'contact.php';
             </script>";
     }else{
@@ -43,4 +38,3 @@ if(isset($_POST['submit'])){
     }
 }
 ?>
-
